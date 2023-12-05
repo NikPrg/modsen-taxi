bbcdb86d2a66a47efccd22f8136733bbd31ae7c4
Nikprg <98475612+NikPrg@users.noreply.github.com>
2023-12-05 13:15:15 +0300

    add initiating kafka topics script

---
+++ kafka-init-topics.sh
@@ -0,0 +1,9 @@
+/opt/bitnami/kafka/bin/kafka-topics.sh --bootstrap-server kafka:29092 --list
+
+echo -e 'Creating kafka topics'
+/opt/bitnami/kafka/bin/kafka-topics.sh --bootstrap-server  kafka:29092 --create --if-not-exists --topic driver-info-events --partitions 3 --replication-factor 1
+/opt/bitnami/kafka/bin/kafka-topics.sh --bootstrap-server  kafka:29092 --create --if-not-exists --topic ride-info-events --partitions 3 --replication-factor 1
+/opt/bitnami/kafka/bin/kafka-topics.sh --bootstrap-server  kafka:29092 --create --if-not-exists --topic driver-status-events --partitions 3 --replication-factor 1
+
+echo -e 'Successfully created the following topics:'
+/opt/bitnami/kafka/bin/kafka-topics.sh --bootstrap-server kafka:29092 --list
