e84dab490a0ad40827f3f08cb200ad777e27780d
Nikprg <98475612+NikPrg@users.noreply.github.com>
2023-12-19 18:18:20 +0300

    card-service: add unit tests

---
+++ card-service/src/test/java/com/example/cardservice/util/DataUtil.java
@@ -0,0 +1,81 @@
+package com.example.cardservice.util;
+
+import com.example.cardservice.amqp.message.CardInfoMessage;
+import com.example.cardservice.dto.model.CardDto;
+import com.example.cardservice.dto.request.CardRegistrationDto;
+import com.example.cardservice.dto.response.AllCardsResponse;
+import com.example.cardservice.dto.response.CreateCardResponse;
+import com.example.cardservice.model.Card;
+import com.example.cardservice.model.PassengerCard;
+import com.example.cardservice.model.PassengerInfo;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.UUID;
+
+public class DataUtil {
+
+    public static final Long CARD_ID = 1L;
+    public static final UUID CARD_EXTERNAL_ID = UUID.randomUUID();
+    public static final String CARD_NUMBER = "5532332131234421";
+
+    public static final Long PASSENGER_INFO_ID = 1L;
+    public static final UUID PASSENGER_INFO_EXTERNAL_ID = UUID.randomUUID();
+
+
+    public static Card defaultCard(){
+        return Card.builder()
+                .id(CARD_ID)
+                .externalId(CARD_EXTERNAL_ID)
+                .number(CARD_NUMBER)
+                .build();
+    }
+
+    public static PassengerInfo initPassengerInfo(){
+        return PassengerInfo.builder()
+                .id(PASSENGER_INFO_ID)
+                .externalId(PASSENGER_INFO_EXTERNAL_ID)
+                .build();
+    }
+
+    public static PassengerInfo defaultPassengerInfoWithCard(){
+        List<PassengerCard> passengerCards = new ArrayList<>();
+        passengerCards.add(new PassengerCard(initPassengerInfo(), defaultCard()));
+
+        return PassengerInfo.builder()
+                .id(PASSENGER_INFO_ID)
+                .externalId(PASSENGER_INFO_EXTERNAL_ID)
+                .cards(passengerCards)
+                .build();
+    }
+
+    public static CardRegistrationDto defaultCardRegistrationDto(){
+        return CardRegistrationDto.builder()
+                .number(CARD_NUMBER)
+                .build();
+    }
+
+    public static CreateCardResponse defaultCreateCardResponse(){
+        return CreateCardResponse.builder()
+                .cardExternalId(CARD_EXTERNAL_ID)
+                .build();
+    }
+
+    public static CardInfoMessage defaultCardInfoMessage(){
+        return new CardInfoMessage(CARD_EXTERNAL_ID);
+    }
+
+    public static CardDto defaultCardDto(){
+        return new CardDto(CARD_NUMBER);
+    }
+
+    public static AllCardsResponse defaultAllCardsResponse(){
+        return new AllCardsResponse(List.of(new CardDto(CARD_NUMBER)));
+    }
+
+    public static AllCardsResponse defaultAllCardsResponseEmpty(){
+        return new AllCardsResponse(Collections.emptyList());
+    }
+}
+
